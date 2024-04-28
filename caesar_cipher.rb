daf3355cc42c843554d0fdfd85c8ef8dc53a0716
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-04-29 00:04:17 +0100

    Clean up

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -6,9 +6,7 @@ module CaesarCipher
     sentence.each_char.with_index do |char, index|
       next unless ALPHABET.include?(char.downcase)
       new_index = (ALPHABET.index(char.downcase) + shift_factor) % LAST_INDEX
-      puts new_index
       sentence[index] = is_uppercase(char) ? ALPHABET[new_index].upcase : ALPHABET[new_index]
-      puts sentence
     end
   end
 
