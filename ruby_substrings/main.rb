0704951eb22a1d261d23503276d3f591c74dbd54
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-04-29 21:21:23 +0100

    Refactor main.rb code

--- ruby_substrings/main.rb
+++ ruby_substrings/main.rb
@@ -1,13 +1,23 @@
 require_relative 'substrings'
 
-puts "Give me a message:"
-message =  gets.chomp
-puts "Give me the substrings you want to find within the previous message, one by one:"
-dictionary = []
+def get_input(prompt)
+  puts prompt
+  input = gets.chomp.strip
+  while input.empty?
+    puts "You haven't provided any input. Try again:"
+    input = gets.chomp.strip
+  end
+  input
+end
 
-while !(input = gets.chomp).empty?
+message = get_input("Give me a message:")
+
+dictionary = []
+input = get_input("Give me the substrings you want to find within the previous message, one by one:")
+while !input.empty?
   dictionary << input
-  puts "Your current list is #{dictionary.join(", ")}.\nPress 'Enter' to proceed to find substrings or give me another substring:" 
+  puts "Your current list is '#{dictionary.join(", ")}'.\nPress 'Enter' to proceed to find substrings or give me another substring:"
+  input = gets.chomp.strip 
 end
 
 puts Substrings.substrings(message, dictionary)
