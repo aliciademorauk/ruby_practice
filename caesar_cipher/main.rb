2387095ef695d6b4a30ef0dcfb40ce5a629d03ea
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-04-29 20:04:29 +0100

    Refactor code to follow DRY principles

--- caesar_cipher/main.rb
+++ caesar_cipher/main.rb
@@ -1,8 +1,16 @@
 require_relative 'caesar_cipher'
 
-puts "Give me a message to encrypt (only letters in the alphabet will be encrypted):"
-message =  gets.chomp
-puts "Give me the shift factor:"
-shift = gets.chomp.to_i
+def get_input(prompt)
+  puts prompt
+  input = gets.chomp.strip
+  while input.empty?
+    puts "You haven't provided any input. Try again:"
+    input =  gets.chomp.strip
+  end
+  input
+end
+
+message = get_input("Give me a message to encrypt (only letters in the alphabet will be encrypted):")
+shift = get_input("Give me the shift factor:").to_i
 
 puts CaesarCipher.encrypt(message, shift)
