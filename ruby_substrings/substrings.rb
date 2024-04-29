782e2eec85da1a38aed7d64fb65c0def20474e5b
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-04-29 19:21:47 +0100

    Change implementation
    Use map method on word dictionary, and for each word, use scan method on the text, passing it a regex to match the word in the text.
    Then, count the size of array generated by scan (containg word * times found), and return that (or nil) for each element of the dictionary
    Then .compact to remove the nil values, and .to_h to transform the two-dimensional array into key-value pairs

--- ruby_substrings/substrings.rb
+++ ruby_substrings/substrings.rb
@@ -1,16 +1,9 @@
 module Substrings
   def self.substrings(text, strings_array)
-    count = {}
-    text = text.downcase.split(" ")
-    strings_array.each do |w|
-      text.each do |word|
-        if word.include?(w.downcase)
-          count[w] ||= 0 
-          count[w] += 1 
-        end
-      end
-    end
-    count
+    lowercase_text = text.downcase
+    strings_array.map do |w|
+      count = lowercase_text.scan(/#{w.downcase}/).size
+      [w, count] if count > 0
+    end.compact.to_h
   end
-  
 end
